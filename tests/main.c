9370cd5663399d86029af8c4ed83d6d3b4ee7fb2
Ne02ptzero <louis@ne02ptzero.me>
2017-07-04 13:35:59 +0200

    Fix(Tests): Change tests order

--- tests/main.c
+++ tests/main.c
@@ -11,9 +11,9 @@ int     main(void) {
     register_test_files();
     register_test_category();
     register_test_config();
-    register_test_packer();
     register_test_packer_file();
     register_test_compile();
+    register_test_packer();
 
     m_info("Beginning tests...\n");
     ret = test_all();
